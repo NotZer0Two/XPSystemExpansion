a2850d4d33b5f60d420264cef8de9e320e5ce376
NotZer0Two <lyonfan2010@gmail.com>
2024-09-10 11:27:05 +0200

    Aggiungere i file di progetto.

---
+++ XPSystemExpansion.cs
@@ -0,0 +1,40 @@
+﻿using Exiled.API.Features;
+using PlaceholderAPI.API.Abstract;
+using XPSystem.API;
+using XPSystem.API.StorageProviders;
+using XPSystem.API.StorageProviders.Models;
+
+namespace XPSystemExpansion
+{
+    public class XPSystemExpansion : PlaceholderExpansion
+    {
+        public override string Author { get; set; } = "NotZer0Two";
+        public override string Identifier { get; set; } = "xpsystem";
+        public override string RequiredPlugin { get; set; } = "XPSystem";
+
+        public override string OnRequest(Player player, string param)
+        {
+            if (!XPExtensions.TryParseUserId(player.UserId, out PlayerId id))
+                return null;
+
+            PlayerInfoWrapper xpplayer = XPExtensions.GetPlayerInfo(id);
+
+            switch (param.ToLower())
+            {
+                case "xp":
+                    return xpplayer.XP.ToString();
+
+                case "level":
+                    return xpplayer.Level.ToString();
+
+                case "xpneedednext":
+                    return xpplayer.NeededXPNext.ToString();
+
+                case "xpneededcurrent":
+                    return xpplayer.NeededXPCurrent.ToString();
+            }
+
+            return null;
+        }
+    }
+}
